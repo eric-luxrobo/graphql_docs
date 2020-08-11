015026d0dee6f9d2527da7a5c425e49f415c9b37
walter.jeung <walter.jeung@luxrobo.net>
2020-08-11 12:22:02 +0900

    Update document style

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -3,7 +3,7 @@ export default function Logo() {
   return (
     <img
       alt="Luxrobo Logo"
-      style={{ marginRight: 10, maxWidth: 150 }}
+      style={{ marginRight: 10, maxWidth: 110 }}
       src={"https://uploads.ifdesign.de/Wdg/Profile/9465/logos/medium/wdg_logo_image.png"}
     />
   );
